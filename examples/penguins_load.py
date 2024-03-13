51519fd7f57d353a95c095f0cb8a8cff811e7f5b
JooYoung Seo <jseo1005@illinois.edu>
2024-03-13 14:16:54 -0500

    Add penguins dataset loading and saving functionality

--- examples/penguins_load.py
+++ examples/penguins_load.py
@@ -1,5 +1,11 @@
+# Load required packages
 import seaborn as sns
 
+# Load the data
 data = sns.load_dataset("penguins")
 
+# Take a glimpse at the data
 data.info()
+
+# Save the data as a csv file
+data.to_csv("data/penguins.csv", index=False)
