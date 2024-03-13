292f51bb652af5c8c57b32e12e5067e9010c2be5
JooYoung Seo <jseo1005@illinois.edu>
2024-03-13 14:14:03 -0500

    Refactor gapminder_dataset.py to load and save data as CSV

--- examples/gapminder_dataset.py
+++ examples/gapminder_dataset.py
@@ -1,5 +1,12 @@
+# Load required packages
+import pandas as pd
 from gapminder import gapminder
 
+# Load the data
 data = gapminder
 
+# Take a glimpse at the data
 data.info()
+
+# Save the data as a csv file
+data.to_csv("data/gapminder.csv", index=False)
