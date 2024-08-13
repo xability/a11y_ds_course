537a42036e075040f9aa164e2d8e71c620f55413
Ken Perry <kperry@aph.org>
2024-08-12 22:52:35 -0400

    fixes to code

--- course_materials/day_5/bar_anual_income.py
+++ course_materials/day_5/bar_anual_income.py
@@ -9,14 +9,16 @@ df = pd.read_csv(
     "../../data/Successful_Employment_for_Blind_Iowans_by_Federal_Fiscal_Year.csv"
 )
 
+df['Gender'] = df['Gender'].str.title()
+df = df[df['Annual Earnings'] > 0]
 
 # Create a bar plot using Seaborn
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(10, 6))
 gender_plot = sns.barplot(
     x="Gender",
     y="Annual Earnings",
     data=df,
-    palette=["blue", "pink"],
+    #palette=["blue", "pink"],
 )
 plt.xlabel("Gender")
 plt.ylabel("Average Annual Income")
--- course_materials/day_5/bar_gapminder2.py
+++ course_materials/day_5/bar_gapminder2.py
@@ -6,11 +6,11 @@ import maidr
 # Load the Gapminder dataset
 gapminder = pd.read_csv("../../data/gapminder.csv")
 
+un_df = gapminder[['continent', 'country']].drop_duplicates()
 
-# Bar Plot: Example - Average life expectancy per continent 
 # Count Plot: Example - Count of countries per continent
 plt.figure(figsize=(10, 6))
-count_plot = sns.countplot(x="continent", data=gapminder)
+count_plot = sns.countplot(x="continent", data=un_df)
 plt.title("Count of Countries per Continent")
 plt.xlabel("Continent")
 plt.ylabel("Number of Countries")
